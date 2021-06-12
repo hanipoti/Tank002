2887b3f1d5a7264923c927ba531e2684c1e483df
hanipoti <71120421+hanipoti@users.noreply.github.com>
2021-06-13 03:58:11 +0900

    アイコン追加 BTデバイスを選択できるようにした 前回のデバイスを保存するようにした

--- app/src/main/java/com/wkwkmaker/tank001/MainActivity.java
+++ app/src/main/java/com/wkwkmaker/tank001/MainActivity.java
@@ -1,35 +1,39 @@
 package com.wkwkmaker.tank001;
 
+import android.app.AlertDialog;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
-import android.support.v7.app.AppCompatActivity;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.view.MotionEvent;
-import android.view.View;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.app.AlertDialog;
-import android.content.DialogInterface;
-import android.widget.RadioButton;
-import android.widget.TextView;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import java.io.BufferedWriter;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
-import java.io.OutputStreamWriter;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 import java.util.Timer;
 import java.util.TimerTask;
 import java.util.UUID;
-import java.util.ArrayList;
 
 
-public class MainActivity extends AppCompatActivity  implements View.OnTouchListener {
+public class MainActivity extends AppCompatActivity implements View.OnTouchListener {
 
     private BluetoothAdapter mBtAdapter; // BTアダプタ
     private BluetoothDevice mBtDevice; // BTデバイス
@@ -43,9 +47,14 @@ public class MainActivity extends AppCompatActivity  implements View.OnTouchList
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        timerSend =new Timer();
+        //プリファレンスから前回の接続先をセットする
+        SharedPreferences pref = getSharedPreferences("wkwk_TankExp",MODE_PRIVATE);
+        TextView tvc = findViewById(R.id.textViewConnectBTName);
+        tvc.setText(pref.getString("selectedBTName",""));
 
-        Button btn= findViewById(R.id.button);
+        timerSend = new Timer();
+
+        Button btn = findViewById(R.id.button);
         btn.setOnTouchListener(this);
 
 
@@ -54,21 +63,23 @@ public class MainActivity extends AppCompatActivity  implements View.OnTouchList
             @Override
             public void run() {
                 TextView tvm = findViewById(R.id.textViewMessage);
-                if (tvm.getText().equals("通信中")){
-                    if (((RadioButton)findViewById(R.id.radioButtonOpeModeLR)).isChecked() || ((RadioButton)findViewById(R.id.radioButtonOpeModeUD)).isChecked()){
+                if (tvm.getText().equals("通信中")) {
+                    if (((RadioButton) findViewById(R.id.radioButtonOpeModeLR)).isChecked() || ((RadioButton) findViewById(R.id.radioButtonOpeModeUD)).isChecked()) {
                         String s = "828";
 
-                        if (((RadioButton)findViewById(R.id.radioButtonOpeModeLR)).isChecked()) s = s + "E";
-                        if (((RadioButton)findViewById(R.id.radioButtonOpeModeUD)).isChecked()) s = s + "L";
+                        if (((RadioButton) findViewById(R.id.radioButtonOpeModeLR)).isChecked())
+                            s = s + "E";
+                        if (((RadioButton) findViewById(R.id.radioButtonOpeModeUD)).isChecked())
+                            s = s + "L";
 
-                        s = s + CheckedNo((RadioButton)findViewById(R.id.radioButtonL1Stop),(RadioButton)findViewById(R.id.radioButtonL1Extends),(RadioButton)findViewById(R.id.radioButtonL1Shrink));
-                        s = s + CheckedNo((RadioButton)findViewById(R.id.radioButtonL2Stop),(RadioButton)findViewById(R.id.radioButtonL2Extends),(RadioButton)findViewById(R.id.radioButtonL2Shrink));
-                        s = s + CheckedNo((RadioButton)findViewById(R.id.radioButtonL3Stop),(RadioButton)findViewById(R.id.radioButtonL3Extends),(RadioButton)findViewById(R.id.radioButtonL3Shrink));
-                        s = s + CheckedNo((RadioButton)findViewById(R.id.radioButtonL4Stop),(RadioButton)findViewById(R.id.radioButtonL4Extends),(RadioButton)findViewById(R.id.radioButtonL4Shrink));
-                        s = s + CheckedNo((RadioButton)findViewById(R.id.radioButtonR1Stop),(RadioButton)findViewById(R.id.radioButtonR1Extends),(RadioButton)findViewById(R.id.radioButtonR1Shrink));
-                        s = s + CheckedNo((RadioButton)findViewById(R.id.radioButtonR2Stop),(RadioButton)findViewById(R.id.radioButtonR2Extends),(RadioButton)findViewById(R.id.radioButtonR2Shrink));
-                        s = s + CheckedNo((RadioButton)findViewById(R.id.radioButtonR3Stop),(RadioButton)findViewById(R.id.radioButtonR3Extends),(RadioButton)findViewById(R.id.radioButtonR3Shrink));
-                        s = s + CheckedNo((RadioButton)findViewById(R.id.radioButtonR4Stop),(RadioButton)findViewById(R.id.radioButtonR4Extends),(RadioButton)findViewById(R.id.radioButtonR4Shrink));
+                        s = s + CheckedNo((RadioButton) findViewById(R.id.radioButtonL1Stop), (RadioButton) findViewById(R.id.radioButtonL1Extends), (RadioButton) findViewById(R.id.radioButtonL1Shrink));
+                        s = s + CheckedNo((RadioButton) findViewById(R.id.radioButtonL2Stop), (RadioButton) findViewById(R.id.radioButtonL2Extends), (RadioButton) findViewById(R.id.radioButtonL2Shrink));
+                        s = s + CheckedNo((RadioButton) findViewById(R.id.radioButtonL3Stop), (RadioButton) findViewById(R.id.radioButtonL3Extends), (RadioButton) findViewById(R.id.radioButtonL3Shrink));
+                        s = s + CheckedNo((RadioButton) findViewById(R.id.radioButtonL4Stop), (RadioButton) findViewById(R.id.radioButtonL4Extends), (RadioButton) findViewById(R.id.radioButtonL4Shrink));
+                        s = s + CheckedNo((RadioButton) findViewById(R.id.radioButtonR1Stop), (RadioButton) findViewById(R.id.radioButtonR1Extends), (RadioButton) findViewById(R.id.radioButtonR1Shrink));
+                        s = s + CheckedNo((RadioButton) findViewById(R.id.radioButtonR2Stop), (RadioButton) findViewById(R.id.radioButtonR2Extends), (RadioButton) findViewById(R.id.radioButtonR2Shrink));
+                        s = s + CheckedNo((RadioButton) findViewById(R.id.radioButtonR3Stop), (RadioButton) findViewById(R.id.radioButtonR3Extends), (RadioButton) findViewById(R.id.radioButtonR3Shrink));
+                        s = s + CheckedNo((RadioButton) findViewById(R.id.radioButtonR4Stop), (RadioButton) findViewById(R.id.radioButtonR4Extends), (RadioButton) findViewById(R.id.radioButtonR4Shrink));
                         s = s + "9";
                         try {
                             writer.write(s);
@@ -85,6 +96,9 @@ public class MainActivity extends AppCompatActivity  implements View.OnTouchList
         MessageBox("未接続");
     }
 
+
+
+
     // アクションバーを表示するメソッド
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -93,56 +107,39 @@ public class MainActivity extends AppCompatActivity  implements View.OnTouchList
         return true;
     }
 
+
     // オプションメニューのアイテムが選択されたときに呼び出されるメソッド
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
-            case R.id.item1:    //接続
+            case R.id.item_connect:    //接続
                 // BTの準備 --------------------------------------------------------------
-                MessageBox("接続中");
+                MessageBox("接続準備中");
 
                 // BTアダプタのインスタンスを取得
                 mBtAdapter = BluetoothAdapter.getDefaultAdapter();
                 if (mBtAdapter == null) {
-                    MessageBox("アダプタの取得失敗");
+                    MessageBox("BTアダプタの取得失敗null");
+                    return true;
                 }
                 if (!mBtAdapter.isEnabled()) {
-                    MessageBox("アダプタの取得失敗");
+                    MessageBox("Bluetoothが無効になってます");
+                    return true;
                 }
 
-                // 相手先BTデバイスのインスタンスを取得
+                //BTデバイス名を取得
+                String BTName = (String) ((TextView) findViewById(R.id.textViewConnectBTName)).getText();
+
+                // BTデバイスのインスタンスを取得
                 Set<BluetoothDevice> bondedDevices = mBtAdapter.getBondedDevices();
-
-//                ArrayList<String> devices = new ArrayList<>();
-//                for (BluetoothDevice dev : bondedDevices) {
-//                    devices.add(dev.getName());
-//                }
-//                String[] items = devices.toArray(new String[devices.size()]);
-//
-//                int whitchBtn = 0;
-//
-//                new AlertDialog.Builder(this)
-//                        .setTitle("接続先")
-//                        .setItems(items, new DialogInterface.OnClickListener(){
-//                            @Override
-//                            public void onClick(DialogInterface dialog, int which) {
-//                                for (BluetoothDevice dev : bondedDevices) {
-//                                    if (dev.getName().equals(items[which])) {
-//                                        mBtDevice = dev;
-//                                    }
-//                                }
-//                            }
-//                        })
-//                        .show();
-
-
                 for (BluetoothDevice dev : bondedDevices) {
-                    if (dev.getName().equals("WK-TANK4-1")) {
+                    if (dev.getName().equals(BTName)) {
                         mBtDevice = dev;
                     }
                 }
                 if (mBtDevice == null) {
-                    MessageBox("デバイスの取得失敗(WK-TANK4-1)");
+                    MessageBox("デバイスが見つからない(" + BTName + ")");
+                    return true;
                 }
 
                 // BTソケットのインスタンスを取得
@@ -166,7 +163,8 @@ public class MainActivity extends AppCompatActivity  implements View.OnTouchList
                 }
 
                 return true;
-            case R.id.item2:
+
+            case R.id.item_disconnect:
                 // ソケットを閉じる
                 MessageBox("未接続");
                 try {
@@ -177,24 +175,55 @@ public class MainActivity extends AppCompatActivity  implements View.OnTouchList
 
                 return true;
 
+
+            case R.id.item_BTSelect:
+                // BTアダプタのインスタンスを取得
+                mBtAdapter = BluetoothAdapter.getDefaultAdapter();
+                // BTデバイスの名前をリストに入れる
+                List<String> BTlist = new ArrayList<>();
+                Set<BluetoothDevice> bondedDevicesB = mBtAdapter.getBondedDevices();
+                for (BluetoothDevice dev : bondedDevicesB) {
+                    BTlist.add(dev.getName());
+                }
+                //Listから文字配列に変換
+                String[] BTListS = BTlist.toArray(new String[BTlist.size()]);
+
+                //ダイアログ(リスト)の表示
+                new AlertDialog.Builder(MainActivity.this)
+                        .setTitle("接続先")
+                        .setItems(BTListS, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                //選択したアイテムを接続先として設定する
+                                TextView tvc = findViewById(R.id.textViewConnectBTName);
+                                tvc.setText(BTListS[which]);
+                                //接続先名をプリファレンスに保存する
+                                SharedPreferences pref = getSharedPreferences("wkwk_TankExp", MODE_PRIVATE);
+                                SharedPreferences.Editor editor = pref.edit();
+                                editor.putString("selectedBTName", BTListS[which]);
+                                editor.commit();
+                            }
+                        })
+                        .show();
+                return true;
+
         }
         return super.onOptionsItemSelected(item);
     }
 
-    private void MessageBox(String str){
-        TextView tvm= findViewById(R.id.textViewMessage);
+    private void MessageBox(String str) {
+        TextView tvm = findViewById(R.id.textViewMessage);
         tvm.setText(str);
 
     }
 
-    private String CheckedNo(RadioButton rb1,RadioButton rb2,RadioButton rb3){
-        if (rb1.isChecked()){
+    private String CheckedNo(RadioButton rb1, RadioButton rb2, RadioButton rb3) {
+        if (rb1.isChecked()) {
             return "0";
         }
-        if (rb2.isChecked()){
+        if (rb2.isChecked()) {
             return "1";
         }
-        if (rb3.isChecked()){
+        if (rb3.isChecked()) {
             return "2";
         }
         return "9";
